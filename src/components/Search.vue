b3277a80acaf114d1bb4a6aa128a182e4361b998
cihatdev <cihat02020202@gmail.com>
2020-07-26 00:31:01 +0300

    initial commit

---
+++ src/components/Search.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="search">
+    <input
+      type="text"
+      v-model="query"
+      @keypress.enter="handleSearch"
+      placeholder="Please Write Here..."
+    />
+    <button @click="handleSearch">Search</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      query: "",
+    };
+  },
+  methods: {
+    handleSearch() {
+      this.$emit("SearchRequested", this.query);
+    },
+  },
+};
+</script>
+
+<style>
+body {
+  background-color: rgb(240, 236, 236);
+}
+.search {
+  text-align: center;
+  margin: 50px 5px;
+}
+.search input {
+  padding: 10px 20px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  width: 400px;
+}
+.search button {
+  padding: 5px 15px;
+  border: 1px solid #ccc;
+  background-color: #4183c4;
+  color: #fff;
+  font-size: 16px;
+  border-radius: 5px;
+}
+</style>
